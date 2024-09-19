8883a1419b3a2bcec494613586f5cec8afaa88c2
Yasser SAIDI <yasersaidi.ys@gmail.com>
2024-09-19 16:11:25 +0100

    initial commit with a started configs.

---
+++ components/my-first-component.tsx
@@ -0,0 +1,9 @@
+const MyFirstComponent = () => {
+	return (
+		<div className="mx-auto flex min-h-screen max-w-5xl animate-pulse flex-col items-center justify-center font-mono text-4xl text-green-500">
+			Get started with this Base Next js App Settings
+		</div>
+	);
+};
+
+export default MyFirstComponent;
